2b8992fa07a79030b1726f8a788c413de624ffac
Enes Dogrul <dogrulenes0@gmail.com>
2026-05-12 13:06:24 +0300

    Harita çizdirildi

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,38 @@
 #include <iostream>
 int main()
 {
-    unsigned int width = 640;
-    unsigned int height = 360;
+    unsigned int width = 1920;
+    unsigned int height = 1080;
     sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({width,height}),"Tutorials");
-    sf::CircleShape circle;
-    circle.setRadius(50.0f);
-    circle.setFillColor(sf::Color::Green);
-    circle.setOrigin({40.f, 40.f});
-    circle.setPosition({300.f,200.f});
-    circle.setOutlineColor(sf::Color::Red);
-    circle.setOutlineThickness(5.0f);
+    sf::RectangleShape dikdortgen;
+    sf::CircleShape player(20.f);
+    player.setFillColor(sf::Color::Green);
+    player.setOrigin({20.f,20.f});
+    dikdortgen.setSize({40.f,40.f});
+    dikdortgen.setOutlineColor(sf::Color::Red);
+    dikdortgen.setOutlineThickness(2.f);
+    dikdortgen.setFillColor(sf::Color::Blue);
+    int harita[15][20] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1},
+        {1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+        {1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1},
+        {1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1},
+        {1, 2, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1},
+        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
     while (window -> isOpen())
     {
+
+
         while (const std::optional event = window ->pollEvent())
         {
             if(event->is<sf::Event::Closed>()){
@@ -30,7 +50,23 @@ int main()
         } 
         
         window->clear();
-        window->draw(circle);
+        for (size_t y = 0; y < 15; y++)
+        {
+            for (size_t x = 0; x < 20; x++)
+            {
+                if(harita[y][x] == 1){
+                    dikdortgen.setPosition({x*40.f,y*40.f});
+                }
+                if(harita[y][x] == 2){
+
+                    player.setPosition({((x*40.f)+20.f),((y*40.f)+20.f)});
+                }
+                window->draw(dikdortgen);
+
+            }
+            window->draw(player);
+        }
+        
         window->display();
         
     }
